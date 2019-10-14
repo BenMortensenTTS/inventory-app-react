ed7461a52dc8a61f64d6fabd0c2741f737d45dae
BenMortensenTTS <ttttloading@gmail.com>
2019-10-14 16:42:01 -0700

    converted app to use hooks

--- src/App.js
+++ src/App.js
@@ -1,67 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import ShowAllStockItems from './components/ShowAllStockItems';
 import ShowStockItem from './components/ShowStockItem';
 import StockItemForm from './components/StockItemForm';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      inventoryData: []
-    }
-  }
+function App() {
+  const [inventoryData, setInventoryData] = useState([]);
 
-  getDataFromAPI = () => {
+  const getDataFromAPI = () => {
     fetch("http://localhost:8080/stockitems")
     .then((res)=> res.json())
     .then((response) => {
-      this.setState({inventoryData: response});
+      setInventoryData(response);
     });
   }
 
-  componentDidMount() {
-    this.getDataFromAPI();
-  }
+  useEffect(()=> {
+    getDataFromAPI();
+  }, [])
 
-  render() {
-    return (
-      <div>
-        <Router>
-          <div>
-            <nav>
-              <ul>
-                <li>
-                  <Link to="/">Show Inventory</Link>
-                </li>
-                <li>
-                  <Link to="/add">Add Inventory Item</Link>
-                </li>
-              </ul>
-            </nav>
+  return (
+    <div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Show Inventory</Link>
+              </li>
+              <li>
+                <Link to="/add">Add Inventory Item</Link>
+              </li>
+            </ul>
+          </nav>
 
-            {/* A <Switch> looks through its children <Route>s and
-                renders the first one that matches the current URL. */}
-            <Switch>
-              <Route path="/stockitem/:id" render={(props)=> (
-                <ShowStockItem {...props} getDataFromAPI={this.getDataFromAPI}/>
+          {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/stockitem/:id" render={(props)=> (
+              <ShowStockItem {...props} getDataFromAPI={getDataFromAPI}/>
+            )}/>
+            <Route path="/edit/stockitem/:id" render={(props)=> (
+              <StockItemForm {...props} action="update" getDataFromAPI={getDataFromAPI} />
+            )} />
+            <Route path="/add" render={(props) => (
+              <StockItemForm {...props} action="submit" getDataFromAPI={getDataFromAPI}/>
               )}/>
-              <Route path="/edit/stockitem/:id" render={(props)=> (
-                <StockItemForm {...props} action="update" getDataFromAPI={this.getDataFromAPI} />
-              )} />
-              <Route path="/add" render={(props) => (
-                <StockItemForm {...props} action="submit" getDataFromAPI={this.getDataFromAPI}/>
-                )}/>
-              <Route exact path="/">
-                <ShowAllStockItems getDataFromAPI={this.getDataFromAPI} stock={this.state.inventoryData}/>
-              </Route>
-            </Switch>
-          </div>
-        </Router>
-      </div>
-    );
-  }
+            <Route exact path="/">
+              <ShowAllStockItems stock={inventoryData}/>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
+  
 }
 
 export default App;
