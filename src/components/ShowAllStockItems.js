478174944984c5773abdaaec0fab430d0d4a1de3
BenMortensenTTS <ttttloading@gmail.com>
2019-10-17 01:21:05 -0700

    added boxes

--- src/components/ShowAllStockItems.js
+++ src/components/ShowAllStockItems.js
@@ -7,8 +7,8 @@ function ShowAllStockItems(props) {
 
 	let itemStockArr = props.stock.map((item)=> {
 		return (
-			<div id="item-box">
-				<Link className="container ui" to={"/stockitem/" + item.id} key={item.id}>
+			<div id="item-box" key={item.id}>
+				<Link className="container ui" to={"/stockitem/" + item.id} >
 					<div className="items">
 						<div className="fields">{item.itemName}</div>
 						<div className="fields">Stock: {item.amountInStock}</div>
