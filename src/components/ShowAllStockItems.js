499248c51a67eb6404bf1ad4d0d7fa343ae3ff30
BenMortensenTTS <ttttloading@gmail.com>
2019-10-17 14:14:34 -0700

    added plus and minus buttons

--- src/components/ShowAllStockItems.js
+++ src/components/ShowAllStockItems.js
@@ -8,12 +8,14 @@ function ShowAllStockItems(props) {
 	let itemStockArr = props.stock.map((item)=> {
 		return (
 			<div id="item-box" key={item.id}>
-				<Link className="container ui" to={"/stockitem/" + item.id} >
+				<Link style={{ textDecoration: 'none'}} className="container" to={"/stockitem/" + item.id} >
 					<div className="items">
 						<div className="fields">{item.itemName}</div>
 						<div className="fields">Stock: {item.amountInStock}</div>
+
 					</div>
 				</Link>
+				<div className="circle-state" id="minus">-</div><span className="circle-state" id="plus">+</span>
 			</div>
 		);
 	})
